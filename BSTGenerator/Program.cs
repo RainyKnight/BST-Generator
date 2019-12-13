c15179c2dee912fc0d404e133b68248414fc6a1a
Lucas Stoltman Mac <stoltmanlucas@gmail.com>
2019-12-12 21:31:26 -0800

    Added some code to get it working, but still nowhere near complete.

--- BSTGenerator/Program.cs
+++ BSTGenerator/Program.cs
@@ -6,9 +6,9 @@ namespace BSTGenerator
 
     class Node
     {
-        public int Data { get; set; }
-        public Node LeftChild { get; set; }
-        public Node RightChild { get; set; }
+        public int Data;
+        public Node LeftChild;
+        public Node RightChild;
 
         public Node() //default constructor
         {
@@ -25,22 +25,23 @@ namespace BSTGenerator
 
     class Tree
     {
-        public Node root { get; set; }
-
-        public void add(int value)
+        public Node insert(Node root, int v)
         {
-            Node before = null;
-            Node after = this.root;
-
-            while (after != null)
+            if (root == null)
             {
-                before = after;
-                if(value < after.Data)
-                    if (value < after.Data) //Is new node in left tree? 
-                        after = after.LeftChild;
-                    else if (value > after.Data)//Is new node in right tree?
-                        after = after.RightChild;
+                root = new Node();
+                root.Data = v;
             }
+            else if (v < root.Data)
+            {
+                root.LeftChild = insert(root.LeftChild, v);
+            }
+            else
+            {
+                root.RightChild = insert(root.RightChild, v);
+            }
+
+            return root;
         }
     }
 
@@ -49,23 +50,49 @@ namespace BSTGenerator
         static void Main(string[] args)
         {
             Tree bst = new Tree();
+            Node root = null;
+            Random random = new Random();
+
 
             Console.Write("How many nodes do you want? ");
             int numNodes = Convert.ToInt32(Console.ReadLine());
 
+            int[] a = new int[numNodes];
+
+            for (int i = 0; i < numNodes; i++)
+            {
+                a[i] = random.Next(100);
+            }
+
+
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-            //TODO BST generating function
+            for (int i = 0; i < numNodes; i++)
+            {
+                bst.insert(root, a[i]);
+
+            }
 
             watch.Stop();
             TimeSpan ts = watch.Elapsed;
+            PrintClock(ts);
 
-            // Format and display the TimeSpan value.
+            foreach (int items in a)
+            {
+                Console.WriteLine(items);
+            }
+
+        }
+
+        // Format and display the TimeSpan value
+        static void PrintClock(TimeSpan time)
+        {
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
+                time.Hours, time.Minutes, time.Seconds,
+                time.Milliseconds / 10);
             Console.WriteLine("RunTime " + elapsedTime);
+
         }
     }
 }
